14bbfe548ebab0e3d8943e1b9e32a4d3baac8339
tosxin-456 <tosinekshally@gmail.com>
2024-08-17 16:36:06 +0100

    feat:getParent

--- controller/userController.js
+++ controller/userController.js
@@ -383,7 +383,8 @@ exports.courseEnrollment = async (req, res) => {
 };
 
 exports.getParentWithNewCourseInvite = async (req, res) => {
-  const { email } = req.body;
+  const { email } = req.user;
+  console.log(email)
 
   const parent = await Parents.findOne({ email });
 
