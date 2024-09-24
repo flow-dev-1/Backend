12566654e2de89b3b5f4cba13a30fdd38bb3a965
tosxin-456 <tosinekshally@gmail.com>
2024-09-24 15:52:49 +0100

    added the educator admin

--- controller/schoolsController.js
+++ controller/schoolsController.js
@@ -109,6 +109,29 @@ exports.getSingleEnrolledCourse = async (req, res) => {
   res.status(StatusCodes.OK).json({ course });
 };
 
+exports.getAllEnrolledCourse = async (req, res) => {
+  let { courseId } = req.params;
+
+  const course = await SchoolCourses.find({ school: req.params.id, status: "Active", course:courseId  })
+    .populate("course", "title image")
+    .populate({
+      path: "studentEnrollments",
+      populate: {
+        path: "user",
+        select: "fullName email phone gender DOB",
+      },
+    });
+
+  // console.log(course)
+
+  course.studentEnrollments = course.studentEnrollments.filter(
+    (item) => item.status === "Confirmed"
+  );
+
+  res.status(StatusCodes.OK).json({ course });
+};
+
+
 exports.getCourselist = async (req, res) => {
   let { enrolledCourseId } = req.params;
 
