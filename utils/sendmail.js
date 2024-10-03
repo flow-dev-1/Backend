db31e34491c8d043cb33b177f8de01bbd0f91ed9
joseph <jossyojih@gmail.com>
2024-10-03 12:46:15 +0100

    Admin forget password

--- utils/sendmail.js
+++ utils/sendmail.js
@@ -114,6 +114,47 @@ exports.admin_invite = async (name, email, token) => {
   }
 };
 
+exports.Admin_Otp_ForgotPassword = async (name, email, otp, token) => {
+  let link =
+    process.env.ENV === "production" ? `https://flow-admin.flowonline.app/forgot-password?t=${token}&c=${otp}` :
+      process.env.ENV === "staging"
+        ? `https://my-flow-dev/forgot-password?t=${token}&c=${otp}`
+        : `http://localhost:3000/forgot-password?t=${token}&c=${otp}`;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: EMAIL_USER,
+      secure: true,
+      auth: {
+        pass: EMAIL_PASS,
+        user: EMAIL,
+      },
+    });
+
+    await transporter.sendMail({
+      from: EMAIL,
+      to: email,
+      subject: " FLOW Reset Password",
+      html: ` <b> Hi ${name} </b></br>
+            <p>We recieved a request to reset the Password on your FLOW Account.</p>
+            </br>
+            <p>Please click or copy this link to complete password reset.</p>
+            </br>
+            </br>
+            <b><a href="${link}">${link}</a></b>
+            </br>
+            </br>
+            <p>Please do not forward this email to others in order to prevent anybody else from accessing your account.</p>   
+            </br>
+            <p>Thanks for helping us keep your account secure. </p>`,
+    });
+    console.log("email sent sucessfully");
+  } catch (error) {
+    console.log(error, "email not sent");
+  }
+};
+
+
 exports.school_admin_invite = async (
   status,
   fullName,
