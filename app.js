14dd0d05a69a758acfe0d84e0b748939349d3da7
joseph <jossyojih@gmail.com>
2024-10-08 11:12:23 +0100

    No cron on staging

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ app.use(morgan('tiny'));
 require("./startup/cors.js")(app);
 require("./startup/db")();
 
-cron.schedule('0 12 * * *', () => {
-    console.log("Running at 12:00 PM every day");
-    courseReminder();
-});
+// cron.schedule('0 12 * * *', () => {
+//     console.log("Running at 12:00 PM every day");
+//     courseReminder();
+// });
 
-cron.schedule('*/10 * * * *', () => {
-    console.log("Running every 10 minutes");
-    welcome_new_user(
-        "Jossy",
-        "FLS1234",
-        "jossyojih@gmail.com"
-    );
-});
+// cron.schedule('*/10 * * * *', () => {
+//     console.log("Running every 10 minutes");
+//     welcome_new_user(
+//         "Jossy",
+//         "FLS1234",
+//         "jossyojih@gmail.com"
+//     );
+// });
 
 require("./startup/routes")(app);
 
